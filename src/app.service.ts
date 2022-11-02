fda56ba3af70167949d9f732dfbedccdc5be4e42
cewb <dr.cewb@gmail.com>
2022-11-02 10:10:59 +0300

    set up docker files, nodemon, .env

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello World port: ' + process.env.PORT + " pg port: "
+      + process.env.POSTGRES_PORT;
   }
 }
